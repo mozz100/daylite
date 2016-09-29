1f7a59072b596216e9c0e56ac5d12914f314e1cc
Richard Morrison <richard@rmorrison.net>
2016-09-29 08:52:21 +0100

    More default tweaks

--- color.py
+++ color.py
@@ -7,7 +7,7 @@ LOCATION = Location(('Cambridge', 'Europe', 52.2053, 0.1218, 'Europe/London', 6)
 # See http://www.developers.meethue.com/documentation/core-concepts
 
 WARM = 500      # ct value. Max poss value is 500. Too warm?
-COLD = 242      # ct value. Min poss value is 153.
+COLD = 180      # ct value. Min poss value is 153.
 EARLY = 3       # at 3am (UTC) jump from warm to cold for morning
 START_SLOPE = 3 # hours before dusk
 END_SLOPE = 1.0 # hours after dusk
